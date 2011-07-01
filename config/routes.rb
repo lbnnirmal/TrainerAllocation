67e4e15eb1ed80e9fb3c7baad3f8df2d06bd6b25
Nirmal Balasooriya <lbnnirmal@gmail.com>
2011-07-01 10:12:50 +0530

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.help    '/help',    :controller => 'pages', :action => 'help'	
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.signup '/signup',   :controller => 'users', :action => 'new'
 	
   # The priority is based upon order of creation: first created -> highest priority.
 
